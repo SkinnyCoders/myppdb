14c36129c5e38bd7a2595075391e6aa418c679af
Skinny Coders <skinnycoders@gmail.com>
2020-02-11 16:25:25 +0700

    Penambahan rekap Peserta

--- application/libraries/Pdf.php
+++ application/libraries/Pdf.php
@@ -46,6 +46,6 @@ class Pdf extends Dompdf{
         // Render the PDF
         $this->render();
             // Output the generated PDF to Browser
-        $this->stream($this->filename, array("Attachment" => FALSE));
+        $this->stream($this->filename, array("Attachment" => TRUE));
     }
 }
